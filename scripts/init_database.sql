37688fd1f1f4626c1a529dc63ca84a092aa9116c
Manasipotade <manapotade@gmail.com>
2025-03-10 11:24:31 +0000

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,12 @@
+/*
+Script Purpose:
+  The script sets up three schemas within the database: 'bronze', 'silver', and 'gold'.
+*/
+
+-- Create Schemas
+
+CREATE SCHEMA bronze;
+
+CREATE SCHEMA silver;
+
+CREATE SCHEMA gold;
