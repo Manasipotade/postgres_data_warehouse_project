7a7adbb6e6022105e554d0cf509ccf0c9c72d1a2
Manasipotade <manapotade@gmail.com>
2025-03-31 18:27:17 +0100

    match the structure and style of proc_load_silver.sql

--- scripts/bronze_table/proc_load_bronze.sql
+++ scripts/bronze_table/proc_load_bronze.sql
@@ -4,7 +4,7 @@ Stored Procedure: Load Bronze Layer (Source -> Bronze)
 ===============================================================================
 Script Purpose:
     This stored procedure loads data into the 'bronze' schema from external CSV files. 
-    It performs the following actions:
+    Actions Performed:
     - Truncates the bronze tables before loading data.
     - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
 
@@ -21,68 +21,116 @@ CREATE OR REPLACE PROCEDURE bronze.load_bronze()
 LANGUAGE plpgsql 
 AS $$
 DECLARE 
-    batch_start_time TIMESTAMP;
-    batch_end_time TIMESTAMP;
+    batch_start TIMESTAMP;
+    batch_end TIMESTAMP;
     table_start_time TIMESTAMP;
     table_end_time TIMESTAMP;
     error_message TEXT;
     error_state TEXT;
 BEGIN
+    RAISE NOTICE '>> Starting Bronze Layer Load...';
+    batch_start := clock_timestamp();
+    
     BEGIN
-        RAISE NOTICE '>> Starting Batch Load for All Tables';
-        batch_start_time := clock_timestamp();  -- Capture batch start time
+        ---------------------------
+        -- bronze.crm_cust_info
+        ---------------------------
 
-        -- Load bronze.crm_cust_info
-        RAISE NOTICE '>> Truncating and Loading: bronze.crm_cust_info';
-        table_start_time := clock_timestamp();
+        RAISE NOTICE '>> Truncating: bronze.crm_cust_info';
         TRUNCATE TABLE bronze.crm_cust_info;
+        
+        RAISE NOTICE '>> Inserting into: bronze.crm_cust_info';
+        
+        table_start_time := clock_timestamp();
+        
         COPY bronze.crm_cust_info FROM '/tmp/cust_info.csv' WITH CSV HEADER;
+        
         table_end_time := clock_timestamp();
-        RAISE NOTICE '>> Table Load Time: bronze.crm_cust_info - % seconds', EXTRACT(EPOCH FROM (table_end_time - table_start_time));
+        RAISE NOTICE 'Loaded bronze.crm_cust_info in % seconds', EXTRACT(SECOND FROM table_end - table_start);
 
-        -- Load bronze.crm_prd_info
-        RAISE NOTICE '>> Truncating and Loading: bronze.crm_prd_info';
-        table_start_time := clock_timestamp();
+        ---------------------------
+        -- bronze.crm_prd_info
+        ---------------------------
+
+        RAISE NOTICE '>> Truncating: bronze.crm_prd_info';
         TRUNCATE TABLE bronze.crm_prd_info;
+        
+        RAISE NOTICE '>> Inserting into: bronze.crm_prd_info';
+        
+        table_start_time := clock_timestamp();
+        
         COPY bronze.crm_prd_info FROM '/tmp/prd_info.csv' WITH CSV HEADER;
+        
         table_end_time := clock_timestamp();
-        RAISE NOTICE '>> Table Load Time: bronze.crm_prd_info - % seconds', EXTRACT(EPOCH FROM (table_end_time - table_start_time));
+        RAISE NOTICE 'Loaded bronze.crm_prd_info in % seconds', EXTRACT(SECOND FROM table_end_time - table_start_time_time);
 
-        -- Load bronze.crm_sales_details
-        RAISE NOTICE '>> Truncating and Loading: bronze.crm_sales_details';
+        ---------------------------
+        -- bronze.crm_sales_details
+        ---------------------------
+
+        RAISE NOTICE '>> Truncating: bronze.crm_sales_details';
+        TRUNCATE TABLE bronze.crm_sales_details;        
+        
+        RAISE NOTICE '>> Inserting into: bronze.crm_sales_details';    
+        
         table_start_time := clock_timestamp();
-        TRUNCATE TABLE bronze.crm_sales_details;
+        
         COPY bronze.crm_sales_details FROM '/tmp/sales_details.csv' WITH CSV HEADER;
+        
         table_end_time := clock_timestamp();
-        RAISE NOTICE '>> Table Load Time: bronze.crm_sales_details - % seconds', EXTRACT(EPOCH FROM (table_end_time - table_start_time));
+        RAISE NOTICE '  Loaded bronze.crm_sales_details in % seconds', EXTRACT(SECOND FROM table_end_time - table_start_time_time);
 
-        -- Load bronze.erp_cust_az12
-        RAISE NOTICE '>> Truncating and Loading: bronze.erp_cust_az12';
+        ---------------------------
+        -- bronze.erp_cust_az12
+        ---------------------------
+        
+        RAISE NOTICE '>> Truncating: bronze.erp_cust_az12';
+        TRUNCATE TABLE bronze.erp_cust_az12;        
+        
         table_start_time := clock_timestamp();
-        TRUNCATE TABLE bronze.erp_cust_az12;
+        
+        RAISE NOTICE '>> Inserting into: bronze.erp_cust_az12';
+        
         COPY bronze.erp_cust_az12 FROM '/tmp/CUST_AZ12.csv' WITH CSV HEADER;
+        
         table_end_time := clock_timestamp();
-        RAISE NOTICE '>> Table Load Time: bronze.erp_cust_az12 - % seconds', EXTRACT(EPOCH FROM (table_end_time - table_start_time));
+        RAISE NOTICE '  Loaded bronze.erp_cust_az12 in % seconds', EXTRACT(SECOND FROM table_end_time - table_start_time_time);
 
-        -- Load bronze.loc_a101
-        RAISE NOTICE '>> Truncating and Loading: bronze.loc_a101';
+        ---------------------------
+        -- bronze.erp_loc_a101
+        ---------------------------
+        
+        RAISE NOTICE '>> Truncating: bronze.erp_loc_a101';
+        TRUNCATE TABLE bronze.erp_loc_a101;
+
+        RAISE NOTICE '>> Inserting into: bronze.erp_loc_a101';
+        
         table_start_time := clock_timestamp();
-        TRUNCATE TABLE bronze.loc_a101;
-        COPY bronze.loc_a101 FROM '/tmp/LOC_A101.csv' WITH CSV HEADER;
+        
+        COPY bronze.erp_loc_a101 FROM '/tmp/LOC_A101.csv' WITH CSV HEADER;
+        
         table_end_time := clock_timestamp();
-        RAISE NOTICE '>> Table Load Time: bronze.loc_a101 - % seconds', EXTRACT(EPOCH FROM (table_end_time - table_start_time));
+        RAISE NOTICE '  Loaded bronze.erp_loc_a101 in % seconds', EXTRACT(SECOND FROM table_end_time - table_start_time_time);
 
-        -- Load bronze.erp_px_cat_g1v2
-        RAISE NOTICE '>> Truncating and Loading: bronze.erp_px_cat_g1v2';
-        table_start_time := clock_timestamp();
+        ---------------------------
+        -- bronze.erp_px_cat_g1v2
+        ---------------------------
+
+        RAISE NOTICE '>> Truncating: bronze.erp_px_cat_g1v2';
         TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-        COPY bronze.erp_px_cat_g1v2 FROM '/tmp/PX_CAT_G1V2.csv' WITH CSV HEADER;
-        table_end_time := clock_timestamp();
-        RAISE NOTICE '>> Table Load Time: bronze.erp_px_cat_g1v2 - % seconds', EXTRACT(EPOCH FROM (table_end_time - table_start_time));
 
-        -- Capture end time and print total batch time
-        batch_end_time := clock_timestamp();
-        RAISE NOTICE '>> Total Batch Load Time: % seconds', EXTRACT(EPOCH FROM (batch_end_time - batch_start_time));
+        RAISE NOTICE '>> Inserting into: bronze.erp_px_cat_g1v2';        table_start_time := clock_timestamp();
+        
+        COPY bronze.erp_px_cat_g1v2 FROM '/tmp/PX_CAT_G1V2.csv' WITH CSV HEADER;
+        
+        table_end_time := clock_timestamp();
+        RAISE NOTICE '  Loaded bronze.erp_px_cat_g1v2 in % seconds', EXTRACT(SECOND FROM table_end_time - table_start_time);
+    
+        ---------------------------
+        -- Batch End
+        ---------------------------    
+        batch_end := clock_timestamp();
+        RAISE NOTICE 'Total Batch Load Time: % seconds', ROUND(EXTRACT(EPOCH FROM (batch_end - batch_start))::NUMERIC, 2);
 
     EXCEPTION
         WHEN OTHERS THEN
